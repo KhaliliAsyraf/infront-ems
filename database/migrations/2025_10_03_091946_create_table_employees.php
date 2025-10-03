3af569c714b36baecb560c304875ac478c8b6c9d
Khalili <khaliliasyraf@gmail.com>
2025-10-03 17:28:07 +0800

    * add migration * add model * add seeder * add filament

---
+++ database/migrations/2025_10_03_091946_create_table_employees.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('position');
+            $table->unsignedBigInteger('id_departments');
+            $table->decimal('salary', 10, 2);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('id_departments')->references('id')->on('departments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
